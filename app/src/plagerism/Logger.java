04ddc6fff312b3cd11fb1387f5bffd0e546d66f1
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2021-03-10 14:44:32 -1000

    removed class files

--- app/src/plagerism/Logger.java
+++ app/src/plagerism/Logger.java
@@ -2,9 +2,15 @@ package plagerism;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.security.SecureRandom;
+
 public class Logger{
 	public static String logfile="D:/test.txt";
-
+	//public static String uuidStack;//add UUID after first full test
+	//public static String projectUUID;
+	//you want a uuid for the install, this should be tracked by all projects opened with that install
+	//install uuid should also be embeded in pastes
+	//you want a uuid for the project, this should be embeded in copy pastes
 	public static LinkedList<Entry> past;
 	public static LinkedList<Entry> future;
 	public static boolean futureSafe=false;
@@ -18,10 +24,10 @@ public class Logger{
 		return new Logger();
 	}
 	public static Logger init(String log){
-			logfile=log;
-			past=new LinkedList<Entry>();
-			future=new LinkedList<Entry>();
-			return new Logger();
+		logfile=log;
+		past=new LinkedList<Entry>();
+		future=new LinkedList<Entry>();
+		return new Logger();
 	}
 	public static Logger add(int pos, String log){
 		return add(pos,pos,log);
@@ -89,5 +95,24 @@ public class Logger{
 		past.push(top);
 		return new Logger();
 	}
+	
+	static byte[] createUUID(){
+		SecureRandom random=new SecureRandom();//I know, I know, a bit over kill for a uuid
+		byte[] inter=random.generateSeed(16);
+		inter[6]=byte((inter[6])|0x40);
+		inter[6]=byte(inter[6]&0x4f);//sets 4 highest bits of 7th byte to 0100 because RFC4122 requires it for some reason
+		inter[8]=byte(inter[8]|0x80);
+		inter[8]=byte(inter[8]&0xbf);//sets 2 highest bits of 9th byte to 10 also for no good reason
+		
+		String ret="";
+		for(int i=0;i<16;i++){
+			if(i==4||i==6||i==8||i==10){
+				ret+='-'; 
+			}
+			ret+=hex(uuid[i]);    
+		}
+		return ret;
+	}
+	
 	
 }
