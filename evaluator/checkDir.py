10f31113b33609d8a80789038711bfc5cd8b8d4d
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-11-01 17:35:08 -1000

    fixed json files

--- evaluator/checkDir.py
+++ evaluator/checkDir.py
@@ -1,7 +1,15 @@
 import os
 import json
 import sys
-
+def addList(dictionary, name):
+    if name not in dictionary:
+        dictionary[name] = []
+def addDict(dictionary, name):
+    if name not in dictionary:
+        dictionary[name] = []
+def safeRemove(dictionary,thing):
+	if thing in dictionary:
+		dictionary.remove(thing)
 def load_and_augment_json_files(directory, parent_dir=None):
 	json_objects = {}
 
@@ -22,20 +30,38 @@ def load_and_augment_json_files(directory, parent_dir=None):
 						data = json.load(json_file)
 						name = os.path.realpath(file_path)
 						json_objects[name]=data
-						print(f"Loaded JSON from '{file_path}'")
+						#print(f"Loaded JSON from '{file_path}'")
 				except Exception as e:
 					print(f"Error loading JSON from '{file_path}': {str(e)}")
 
 	return json_objects
-	
+def processDir(directory):
+	json_objects = load_and_augment_json_files(directory)
+	#print(json_objects)
+	infectionMap={}#machine.projectID.infections (other only)
+	for key in json_objects:
+		#print(json_objects[key]["InfectionStack"])
+		creatorUUID=json_objects[key]["CreatorUUID"]
+		addDict(infectionMap,creatorUUID)
+		projectUUID=json_objects[key]["CreatorUUID"]
+		addDict(infectionMap[creatorUUID],projectUUID)
+		addList(infectionMap[creatorUUID][projectUUID],"install")
+		addList(infectionMap[creatorUUID][projectUUID],"infect")
+		infectList=json_objects[key]["InfectionStack"]
+		safeRemove(infectList,creatorUUID)
+		remove(infectList,projectUUID)
+		installList=json_objects[key]["InstallUUIDStack"]
+		safeRemove(installList,creatorUUID)
+		safeRemove(installList,projectUUID)
+		for i in infectList:
+			infectionMap[creatorUUID][projectUUID]["infect"].append(i)
+		for i in installList:
+			infectionMap[creatorUUID][projectUUID]["install"].append(i)
+		print(infectionMap)
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print("Usage: python "+sys.argv[0]+" /path/to/directory")
 		sys.exit(1)
 
 	directory = sys.argv[1]
-	json_objects = load_and_augment_json_files(directory)
-	print(json_objects)
-	# Now you have a list of JSON objects in json_objects
-	# Each JSON object has a "name" field indicating the directory it was in.
-	# You can work with these objects as needed.
+	processDir(directory)
