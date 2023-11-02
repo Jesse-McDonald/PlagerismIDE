51759f9f5490399e260d2a06bbcf7c2b648b5ec7
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-11-01 18:31:15 -1000

    Update checkDir.py

--- evaluator/checkDir.py
+++ evaluator/checkDir.py
@@ -6,10 +6,18 @@ def addList(dictionary, name):
         dictionary[name] = []
 def addDict(dictionary, name):
     if name not in dictionary:
-        dictionary[name] = []
+        dictionary[name] = {}
 def safeRemove(dictionary,thing):
 	if thing in dictionary:
 		dictionary.remove(thing)
+def subKeyIn(dictionary,subkey,exclude=None):
+	for key in dictionary:
+		if key==exclude:
+			pass
+		if(subkey in dictionary[key]):
+			return key
+		else:
+			return None
 def load_and_augment_json_files(directory, parent_dir=None):
 	json_objects = {}
 
@@ -30,10 +38,11 @@ def load_and_augment_json_files(directory, parent_dir=None):
 						data = json.load(json_file)
 						name = os.path.realpath(file_path)
 						json_objects[name]=data
+						json_objects[name]["name"]=os.path.basename(os.path.dirname(name))
 						#print(f"Loaded JSON from '{file_path}'")
 				except Exception as e:
 					print(f"Error loading JSON from '{file_path}': {str(e)}")
-
+	
 	return json_objects
 def processDir(directory):
 	json_objects = load_and_augment_json_files(directory)
@@ -43,13 +52,18 @@ def processDir(directory):
 		#print(json_objects[key]["InfectionStack"])
 		creatorUUID=json_objects[key]["CreatorUUID"]
 		addDict(infectionMap,creatorUUID)
-		projectUUID=json_objects[key]["CreatorUUID"]
+		addList(infectionMap[creatorUUID],"names")
+		infectionMap[creatorUUID]["names"].append(json_objects[key]["name"] )
+		projectUUID=json_objects[key]["ProjectUUID"] 
 		addDict(infectionMap[creatorUUID],projectUUID)
 		addList(infectionMap[creatorUUID][projectUUID],"install")
+		addList(infectionMap[creatorUUID][projectUUID],"names")
+		infectionMap[creatorUUID][projectUUID]["names"].append(json_objects[key]["name"] )
+		
 		addList(infectionMap[creatorUUID][projectUUID],"infect")
 		infectList=json_objects[key]["InfectionStack"]
 		safeRemove(infectList,creatorUUID)
-		remove(infectList,projectUUID)
+		safeRemove(infectList,projectUUID)
 		installList=json_objects[key]["InstallUUIDStack"]
 		safeRemove(installList,creatorUUID)
 		safeRemove(installList,projectUUID)
@@ -57,7 +71,24 @@ def processDir(directory):
 			infectionMap[creatorUUID][projectUUID]["infect"].append(i)
 		for i in installList:
 			infectionMap[creatorUUID][projectUUID]["install"].append(i)
-		print(infectionMap)
+	for key in json_objects:
+		#print(json_objects[key]["InfectionStack"])
+		creatorUUID=json_objects[key]["CreatorUUID"]
+		projectUUID=json_objects[key]["ProjectUUID"]
+		for i in infectionMap[creatorUUID][projectUUID]["infect"]:
+			if(i in infectionMap ):
+
+				print (json_objects[key]["name"]+" has install infection from "+ str(infectionMap[i]["names"]))
+			skey=subKeyIn(infectionMap,i,creatorUUID)
+			if(not skey is None):
+				print (json_objects[key]["name"]+" has project infection from "+ str(infectionMap[key][i]["names"]))
+		for i in infectionMap[creatorUUID][projectUUID]["install"]:
+			if(i in infectionMap ):
+				print (json_objects[key]["name"]+" has install install infection from "+ str(infectionMap[i]["names"]))
+			skey=subKeyIn(infectionMap,i,creatorUUID)
+			if(not skey is None):
+				print (json_objects[key]["name"]+" has project install infection from "+ str(infectionMap[key][i]["names"]))
+	#print(infectionMap)
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print("Usage: python "+sys.argv[0]+" /path/to/directory")
